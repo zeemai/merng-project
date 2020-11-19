000f014d6c55aef182518e2a8544ab6a08f2f269
zeemai <zee.hao.mai@gmail.com>
2020-11-19 00:19:55 +0000

    set up schema

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config.js");
 
 const typeDefs = gql`
   type Query {
@@ -9,7 +12,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    sayHi: () => "helllllo",
+    sayHi: () => "helllllo!!!",
   },
 };
 
@@ -18,6 +21,12 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("MongoDB connected");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
