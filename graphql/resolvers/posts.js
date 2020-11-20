5fd59374829c28bcb6c33e219cf3e54b48c8d0e6
zeemai <zee.hao.mai@gmail.com>
2020-11-20 17:30:30 +0000

    check for body empty

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -28,6 +28,9 @@ module.exports = {
   Mutation: {
     async createPost(_, { body }, context) {
       const user = checkAuth(context); //already verify
+      if (args.body.trim() === "") {
+        throw new Error("Post Body Must not be empty");
+      }
       const newPost = new Post({
         body,
         user: user.id,
