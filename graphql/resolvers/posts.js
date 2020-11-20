802ec0e043e478325d89a051842530f9ebf30297
zeemai <zee.hao.mai@gmail.com>
2020-11-20 17:00:28 +0000

    like and comment route

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,6 +1,6 @@
 const Post = require("../../models/Post");
 const checkAuth = require("../../util/checkAuth");
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 module.exports = {
   Query: {
@@ -50,6 +50,27 @@ module.exports = {
         }
       } catch (err) {
         throw new Error(err);
+      }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //already liked
+          post.likes = post.likes.filter((like) => like.username !== username);
+          await post.save();
+        } else {
+          //not liked yet
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
       }
     },
   },
